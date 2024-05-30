7d1e6b31bb66d505ca97580994ca8cfcce46cdf5
noumanahmad448 <nouman.ahmad448@gmail.com>
2024-05-30 14:26:25 +0800

    homepage img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     @include('session_msg')
     <div class="relative z-10 pt-48 pb-52 bg-cover bg-center"
-     style="background-image: url({{config('setting.im_wel')}})">
+     style="background-image: url('{{url(config('setting.im_wel'))}})'">
         <div class="absolute h-full w-full bg-black opacity-70 top-0 left-0 z-10"></div>
         <div class="container relative z-20 text-white text-center text-2xl">
             <h2 class="font-bold text-5xl mb-8 langBN">
