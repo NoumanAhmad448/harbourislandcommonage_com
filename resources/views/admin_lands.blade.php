0c6bed8d202dd4fceabb30c2aaa4a5fed79fe157
noumanahmad448 <nouman.ahmad448@gmail.com>
2024-06-19 21:23:31 +0800

    url updated

--- resources/views/admin_lands.blade.php
+++ resources/views/admin_lands.blade.php
@@ -42,7 +42,7 @@
                             <td >
                             @if(count($lands->landFiles) > 0)
                                 @foreach($lands->landFiles as $landFile)
-                                    <a href="{{Storage::url($landFile->link)}}">{{$landFile->f_name}}</a><br/>
+                                    <a href="{{file_path($landFile->link)}}">{{$landFile->f_name}}</a><br/>
                                 @endforeach
                             @endif
                             </td>
